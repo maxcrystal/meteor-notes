39a9f652a642a19d462c4eec37078582c5a8c792
maxcrystal <hmi@ya.ru>
2018-09-06 20:56:14 +0300

    fix cursor jump in editor when typing

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -9,25 +9,44 @@ import { Notes } from './../api/notes';
 
 
 export class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: '',
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const currentNoteId = this.props.note ? this.props.note._id : undefined;
+    const prevNoteId = prevProps.note ? prevProps.note._id : undefined;
+
+    if (currentNoteId && currentNoteId !== prevNoteId) {
+      this.setState({
+        title: this.props.note.title,
+        body: this.props.note.body,
+      });
+    }
+  }
 
   handleBodyChange(e) {
-    Meteor.call('notes.update', this.props.note._id, {
-      body: e.target.value,
-    });
+    const body = e.target.value;
+    this.setState({body});
+    Meteor.call('notes.update', this.props.note._id, {body});
   }
 
   handleTitleChange(e) {
-    Meteor.call('notes.update', this.props.note._id, {
-      title: e.target.value,
-    }); 
+    const title = e.target.value;
+    this.setState({title});
+    Meteor.call('notes.update', this.props.note._id, {title}); 
   }
 
   render() {
     if (this.props.note) {
       return (
         <div>
-          <Input value={this.props.note.title} placeholder="Untitled" onChange={this.handleTitleChange.bind(this)} />
-          <Input type="textarea" value={this.props.note.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)} />
+          <Input value={this.state.title} placeholder="Untitled" onChange={this.handleTitleChange.bind(this)} />
+          <Input type="textarea" value={this.state.body} placeholder="Your note here" onChange={this.handleBodyChange.bind(this)} />
           <Button color="danger">Delete Note</Button>
         </div>
       );
